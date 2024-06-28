a32b904250545dbc80aff686f1c85becf97b279c
SachinKn <106311617+sachinknsachi@users.noreply.github.com>
2024-06-28 11:32:03 +0530

    Corrected JsonPathUtils.

--- src/main/java/libs/utils/JsonPathUtils.java
+++ src/main/java/libs/utils/JsonPathUtils.java
@@ -6,17 +6,19 @@ import io.restassured.path.json.JsonPath;
 
 public class JsonPathUtils {
     
-    JsonPath jsonPath;
+    String filePath;
     
     public JsonPathUtils(String filePath) {
-        jsonPath = new JsonPath(new File(filePath));
+        this.filePath = filePath;
     }
 
     public int getSize(String... pathParams) {
+        JsonPath jsonPath = new JsonPath(new File(filePath));
         return jsonPath.getInt(String.join(".", pathParams) + ".size()");
     }
 
     public JsonPath getJsonPath(String... pathParams) {
+        JsonPath jsonPath = new JsonPath(new File(filePath));
         return jsonPath.setRootPath(String.join(".", pathParams));
     }
 }
